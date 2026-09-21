04f3c88db337240a68aaa24e098692916d5c5283
camijavi <camijavinavarro@gmail.com>
2026-09-21 09:54:19 -0600

    add print functions for error, warning, and success messages; update main function to use printSuccess; implement employee data input

--- components.py
+++ components.py
@@ -1,4 +1,17 @@
 import os
 
+
+def printError(message):
+    print(f"\033[31m{message}\033[0m")
+
+
+def printWarning(message):
+    print(f"\033[33m{message}\033[0m")
+
+
+def printSucess(message):
+    print(f"\033[32m{message}\033[0m")
+
+
 def clearConsole():
     os.system('cls' if os.name == 'nt' else 'clear')
--- main.py
+++ main.py
@@ -1,5 +1,47 @@
-from components import clearConsole
+from components import clearConsole, printError,printSucess, printWarning
+
+
+def readEmployeeData():
+    employeeData = input("Nombre del colaborador: ")
+    basicSalary = float(input("Salario básico: "))
+
+    return employeeData, basicSalary
+
+def calculateBasicDeduction(basicSalary):
+    basicDeductionPercentage = 0.10
+
+    return 
+
+def calculateSocialSecurityContribution(basicSalary):
+    socialSecurityContributionPercentage = 0.
+
+    return basicSalary
+
+def calculateEmploymentIncomeTax(basicSalary):
+    employementIncomeTaxPercentage = 0.0
+
+    return basicSalary
+
+
+def calculateNIO_USD():
+    exchangeRateNIO = 36.76
+
+
+
+
+def calculateNetSalary(basicSalary):
+    return "hello"
+
+
+
+def showEmployeeNetSalary():
+    return "Hell yeah"
+
+
 
 def main ():
     clearConsole
-    print("CALCULADORA DE SALARIOS DE COLABORADORES")
+    printSucess("CALCULADORA DE SALARIOS DE COLABORADORES")
+
+if __name__ == "__main__":
+    main()
